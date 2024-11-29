21a5bb9bf30118af503469c129e4639bc953731f
Stars sea <Stars_sea@outlook.com>
2024-11-29 15:08:01 +0800

    Update build.yml and mirrors

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,8 @@
 pluginManagement {
     repositories {
-        maven { url = uri("https://maven.aliyun.com/nexus/content/repositories/google") }
-        maven { url = uri("https://maven.aliyun.com/nexus/content/groups/public") }
-        maven { url = uri("https://maven.aliyun.com/nexus/content/repositories/jcenter") }
+//        maven { url = uri("https://maven.aliyun.com/nexus/content/repositories/google") }
+//        maven { url = uri("https://maven.aliyun.com/nexus/content/groups/public") }
+//        maven { url = uri("https://maven.aliyun.com/nexus/content/repositories/jcenter") }
 
         google {
             content {
@@ -18,9 +18,9 @@ pluginManagement {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-        maven { url = uri("https://maven.aliyun.com/nexus/content/repositories/google") }
-        maven { url = uri("https://maven.aliyun.com/nexus/content/groups/public") }
-        maven { url = uri("https://maven.aliyun.com/nexus/content/repositories/jcenter") }
+//        maven { url = uri("https://maven.aliyun.com/nexus/content/repositories/google") }
+//        maven { url = uri("https://maven.aliyun.com/nexus/content/groups/public") }
+//        maven { url = uri("https://maven.aliyun.com/nexus/content/repositories/jcenter") }
 
         google()
         mavenCentral()
